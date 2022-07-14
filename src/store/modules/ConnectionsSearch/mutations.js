5042903814f69b33618a40bb7635cc42b364a296
Melvin Schenker <melvin-schenker@hotmail.com>
2022-07-14 10:52:08 +0200

    adjust duration display

--- src/store/modules/ConnectionsSearch/mutations.js
+++ src/store/modules/ConnectionsSearch/mutations.js
@@ -1,19 +1,32 @@
 import get from 'lodash.get';
+import moment from 'moment';
+
+function formatDuration(timeString = '') {
+  const noDaysString = timeString.slice(4);
+  const hours = moment(noDaysString, 'hh:mm:ss').hours();
+  const minutes = moment(noDaysString, 'hh:mm:ss').minutes();
+
+  if (hours === 0) return `${minutes} min`
+
+  return `${hours} h ${minutes} min`
+}
 
 export function addConnectionToState(state, payload) {
   const mappedConnections = payload.data.connections && payload.data.connections.map((connection) => {
     const departure = get(connection, 'from.departure');
     const arrival = get(connection, 'to.arrival');
-    const duration = get(connection, 'duration').slice(3);
-    const journey = get(connection, 'sections[0].journey.category');
-    const platform = get(connection, 'sections[0].departure.platform');
+    const duration = formatDuration(get(connection, 'duration'));
+    const journey = get(connection, 'products[0]');
+    const platform = get(connection, 'from.platform');
+    const sections = get(connection, 'sections');
 
     return ({
       departure,
       arrival,
       duration,
       journey,
-      platform
+      platform,
+      sections
     })
   })
   state.searchQuery = payload.query;
